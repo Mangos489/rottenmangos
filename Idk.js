9e9a838035d2628e6129b5ab50f8c8771f428d87
Ecruz22004 <ecruz22004@gmail.com>
2023-11-03 16:08:03 -0700

    hib

---
+++ Idk.js
@@ -0,0 +1,6 @@
+const clickableBox = document.getElementById('myClickableBox');
+
+clickableBox.addEventListener('click', function() {
+    // Add your code here to define what happens when the box is clicked
+    alert('The box was clicked!');
+});
