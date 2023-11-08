e127a2b52ec2f794cdac65ac6e2f3c3e6e9ab50f
Ecruz22004 <ecruz22004@gmail.com>
2023-11-08 11:04:37 -0800

    Added Svelte
    Working on adding framework. Testing compatibility with github pages.

---
+++ RottenBeta/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<body>
+    <h1>Be not afraid for I have saved you</h1>
+    <h2>For I save all that believe and follow</h2>
+
+    <figure class="centered">
+        <img src="./src/lib/BNA.jpg" alt="Heavenly figure">
+        <figcaption>Do Not Be Afraid</figcaption>
+    </figure>
+
+</body>
+
