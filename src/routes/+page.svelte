80e330fc21370633a7f8f1f727fcda48bcbad476
Ecruz22004 <ecruz22004@gmail.com>
2023-11-10 14:55:21 -0800

    Various Fixes
    So the website is running now. But idk if i made the svelte app.html the main page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,12 @@
 <script>
 	import welcome from '$lib/BNA.jpg';
 	import welcome_fallback from '$lib/BNA.jpg';
-    import 'Style.css';
+    import '$lib/Style.css';
 </script>
 
 <svelte:head>
 	<title>RottenMangos</title>
 	<meta name="2nd page" content="Second Page" />
+	
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-</section>
