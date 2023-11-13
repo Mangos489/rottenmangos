c56e164fe612d7f7e394813ec5d34cedf3fa8e88
Ecruz22004 <ecruz22004@gmail.com>
2023-11-13 14:48:20 -0800

    Maybe this works
    Adding file text that worked on previous version of Rotten 2.0.4

--- Rotten/svelte.config.js
+++ Rotten/svelte.config.js
@@ -10,7 +10,7 @@ const config = {
     kit: {
 		adapter: adapter(),
         paths: {
-            base: process.env.NODE_ENV === 'production' ? '/Ecruz22004.github.io' : '',
+            base: process.env.NODE_ENV === 'production' ? '/Ecruz22004.github.' : '',
         }
     }
 };
