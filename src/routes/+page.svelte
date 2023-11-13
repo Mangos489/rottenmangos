8f4e351001bd9b34a7397b9cd3d2a1418a89aa76
Ecruz22004 <ecruz22004@gmail.com>
2023-11-13 14:52:33 -0800

    restoring past save
    Restored Rotten 2.0.3

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import welcome from '$lib/BNA.jpg';
+	import welcome_fallback from '$lib/BNA.jpg';
+    import '$lib/Style.css';
+</script>
+
+<svelte:head>
+	<title>RottenMangos</title>
+	<meta name="2nd page" content="Second Page" />
+</svelte:head>
+
