751eb6cc2c80a3861df964c912a8603d80cc8a7e
Ecruz22004 <ecruz22004@gmail.com>
2023-11-14 00:28:09 -0800

    Adding my own html to the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,6 @@
 <svelte:head>
-	<title>How to deploy a SvelteKit website to GitHub pages</title>
+	<title>RottenMangos</title>
 </svelte:head>
 
-<h1>How to deploy a SvelteKit website to GitHub pages</h1>
 
-<p>
-	Dolore ipsum esse voluptate. Culpa officia minim duis enim Lorem laboris sunt exercitation commodo
-	non ullamco. Aute eiusmod ullamco qui commodo aliqua cillum non voluptate sunt culpa. Cillum
-	pariatur mollit elit et aute quis dolor ipsum cillum ea nisi ea elit. Qui esse incididunt duis
-	ipsum nulla commodo et ea enim non anim nisi eiusmod cillum et. Mollit ipsum excepteur sint
-	consectetur duis ipsum aute.
-</p>
 
