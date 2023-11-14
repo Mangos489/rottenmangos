e8c0a34b29d7ab85fc92ef8262c09a66b7fb26d5
Ecruz22004 <ecruz22004@gmail.com>
2023-11-13 23:23:14 -0800

    Adding our first web page
    I took the code in original index.html file from 1st version of website alongside importing BNA png.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<body>
+    <h1>Be not afraid for I have saved you</h1>
+    <h2>For I save all that believe and follow</h2>
+
+    <figure class="centered">
+        <img src="./src/lib/BNA.jpg" alt="Heavenly figure">
+        <figcaption>Do Not Be Afraid</figcaption>
+    </figure>
+    
+    <div style="display: flex; justify-content:center;">
+        <a
+        href="./RDR2.html">Redemption</a>
+</body>
