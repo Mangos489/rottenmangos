817fb1b47c5d945eef7be2786ab21c447de09564
Ecruz22004 <126013734+Ecruz22004@users.noreply.github.com>
2023-11-24 20:33:14 -0800

    Pushing to main
    Pushing to main just to avoid clutter. Also note that I need to change the urls manually on the gh pages branch. Regardless ive added the archieve page and will be working on more.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
     import { base } from '$app/paths';
+    import '/src/global.css'
 </script>
  
 <nav>
     <a href="{base}/">Home</a>
-    <a href="{base}/about">About</a>
+    <a href="{base}/Archive">Archive</a>
+    <a href="{base}/Contact">Contact</a>
+    <a href="{base}/Projects">Projects</a>
+    <a href="{base}/About">About me</a>
 </nav>
+
+<main>
+	<slot />
+</main>
---
+++ src/routes/Archive/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+    import Bonnie from '$lib/Bonnie.png';
+    import Death from '$lib/Death.png';
+    import Tokyo from '$lib/Tokyo.png';
+
+    let images = [
+    { src: Bonnie, caption: 'Bonnie from Willythewampus' },
+    { src: Death, caption: 'Patrolling the consecrated snowlands. By ChristianYoung on Instagram' },
+    { src: Tokyo, caption: 'Abandoned tori gate found at the end of a submerged tunnel in Japan.' },
+  ];
+  </script>
+  
+  <style>
+    /* Your styling goes here */
+  
+    .gallery {
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+      gap: 16px;
+      margin: 16px;
+    }
+  
+    .gallery img {
+      width: 100%;
+      height: auto;
+      border-radius: 8px;
+      cursor: pointer;
+      transition: transform 0.3s ease-in-out;
+    }
+  
+    .gallery img:hover {
+      transform: scale(1.1);
+    }
+    .caption {
+    text-align: center;
+    margin-top: 8px;
+    color: white; /* Set the text color to white */
+  }
+  </style>
+  
+  <div class="gallery">
+    {#each images as { src, caption }, index (src)}
+      <div>
+        <img src={src} alt={`Image ${index + 1}`} />
+        <p class="caption">{caption}</p>
+      </div>
+    {/each}
+  </div>
+  
