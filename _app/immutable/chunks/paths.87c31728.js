9060bc3bcaebd1e72d9251287060118acfb38274
Ecruz22004 <126013734+Ecruz22004@users.noreply.github.com>
2023-11-27 18:03:38 -0800

    Updates

---
+++ _app/immutable/chunks/paths.87c31728.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_114hno6)==null?void 0:s.base)??"/rottenmangos.xyz";var t;const e=((t=globalThis.__sveltekit_114hno6)==null?void 0:t.assets)??a;export{e as a,a as b};
