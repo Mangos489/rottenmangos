ba5e4b080785ba39e503cdf2d4d734ad995f6df6
Ecruz22004 <126013734+Ecruz22004@users.noreply.github.com>
2023-11-29 11:46:45 -0800

    Updates

--- _app/immutable/nodes/10.d5c25c8d.js
+++
@@ -1,8 +0,0 @@
-import{s as g,n}from"../chunks/scheduler.63274e7e.js";import{S as v,i as w,g as u,s as f,h as c,x as m,c as _,k as d,a as r,f as l}from"../chunks/index.d32a183c.js";/* empty css                       */function x(p){let t,o='<h3>Completed Projects</h3> <a href="https://rottenmangos.xyz" target="_blank">Personal Website</a>',i,s,h=`<h3>Future Projects</h3> <p>Build a command line tool with rust (next)
-        Build a os with rust
-        possibly making a programming language with rust
-        Building a physics engine with C++
-        Building a shell with c or rust undecided rn 
-        Building a visual recognition system with python
-        Building a webserver with ruby
-        Writing a hashtable with C</p>`;return{c(){t=u("div"),t.innerHTML=o,i=f(),s=u("div"),s.innerHTML=h,this.h()},l(e){t=c(e,"DIV",{class:!0,"data-svelte-h":!0}),m(t)!=="svelte-w949ea"&&(t.innerHTML=o),i=_(e),s=c(e,"DIV",{class:!0,"data-svelte-h":!0}),m(s)!=="svelte-1t1wevq"&&(s.innerHTML=h),this.h()},h(){d(t,"class","container"),d(s,"class","container")},m(e,a){r(e,t,a),r(e,i,a),r(e,s,a)},p:n,i:n,o:n,d(e){e&&(l(t),l(i),l(s))}}}class C extends v{constructor(t){super(),w(this,t,null,x,g,{})}}export{C as component};
