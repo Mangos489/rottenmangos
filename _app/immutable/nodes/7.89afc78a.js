99d6e9fe8a372aa7894a8d64af5871719e0737a0
ecruz22004 <ecruz22004@gmail.com>
2024-01-01 14:08:18 -0800

    Updates

---
+++ _app/immutable/nodes/7.89afc78a.js
@@ -0,0 +1,11 @@
+import{s as i,n as s}from"../chunks/scheduler.e108d1fd.js";import{S as l,i as r,g as p,h as c,x as m,a as f,f as v}from"../chunks/index.4d21fe7b.js";import{i as g}from"../chunks/logo5.3cfd03dd.js";import{b as h}from"../chunks/paths.0b835497.js";function d(o){let e,a=`<p class="top svelte-z9vj97">(Welcome)</p> <div class="containtop svelte-z9vj97"><div class="greycontainer svelte-z9vj97"><div class="boxtxt svelte-z9vj97"><p>Rotten Mangos is a Los Angeles marketing agency that seeks to promote 
+            and elevate local buisness by offering competitve yet affordable 
+            services that align with the interests of our clinetel.</p> <p class="transparent svelte-z9vj97">filler</p> <p class="title svelte-z9vj97">(Our philosophy)</p> <p>Local Businesses are now faced with insurmountable challenges when competing
+             against corporate America. By utilizing affordable and powerful
+             <a style="text-decoration:none; color:#DEC18F;" href="${h}/Archive">tools</a>
+               we seek to curate websites that minimize how many hands touch the income
+               of our clients while also offering affordable service.</p> <p class="transparent svelte-z9vj97">filler</p> <p class="title svelte-z9vj97" style="margin-left: -10%;">(Competition)</p> <p>Professional apperanceses are everything, from ensuring that missed
+            fees are payed to ensuring a smooth transaction of goods. Professional
+            apperances grant a buisness legitamacy, authority but most importantly
+            respect. A professional apperance adds weight to the pentalies placed
+            upon customers when fees are unmet thus ensuring businesses are made whole.</p></div></div> <img class="skull svelte-z9vj97" src="${g}" alt="Img"/></div>`;return{c(){e=p("body"),e.innerHTML=a},l(t){e=c(t,"BODY",{"data-svelte-h":!0}),m(e)!=="svelte-bwgdrf"&&(e.innerHTML=a)},m(t,n){f(t,e,n)},p:s,i:s,o:s,d(t){t&&v(e)}}}class _ extends l{constructor(e){super(),r(this,e,null,d,i,{})}}export{_ as component};
