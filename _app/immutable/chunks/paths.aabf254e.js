62f038df06479450c636a3efe0256250f4be5fec
ecruz22004 <ecruz22004@gmail.com>
2023-12-31 23:06:42 -0800

    Updates

--- _app/immutable/chunks/paths.aabf254e.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_xx2qk1)==null?void 0:s.base)??"/rottenmangos.xyz";var t;const e=((t=globalThis.__sveltekit_xx2qk1)==null?void 0:t.assets)??a;export{e as a,a as b};
