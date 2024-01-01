fda4e44fa6b139331dd99305843d7d50fdb79ec1
ecruz22004 <ecruz22004@gmail.com>
2023-12-31 17:45:56 -0800

    Updates

---
+++ _app/immutable/chunks/logo5.3cfd03dd.js
@@ -0,0 +1,1 @@
+const e=""+new URL("../assets/logo5.bf70beab.png",import.meta.url).href;export{e as i};
---
+++ _app/immutable/nodes/7.35b22e1c.js
@@ -0,0 +1,11 @@
+import{s as i,n as s}from"../chunks/scheduler.e108d1fd.js";import{S as l,i as r,g as p,h as c,x as h,a as m,f}from"../chunks/index.4d21fe7b.js";import{i as g}from"../chunks/logo5.3cfd03dd.js";import{b as d}from"../chunks/paths.aabf254e.js";function u(o){let e,a=`<p class="top svelte-1hpexb2">(Welcome)</p> <div class="containtop svelte-1hpexb2"><div class="greycontainer svelte-1hpexb2"><div class="boxtxt svelte-1hpexb2"><p>Rotten Mangos is a Los Angeles marketing agency that seeks to promote 
+            and elevate local buisness by offering competitve yet affordable 
+            services that align with the interests of our clinetel.</p> <p class="transparent svelte-1hpexb2">filler</p> <p class="title svelte-1hpexb2">(Our philosophy)</p> <p>Local Businesses are now faced with insurmountable challenges when competing
+             against corporate America. By utilizing affordable and powerful
+             <a style="text-decoration:none; color:#DEC18F;" href="${d}/Archive">tools</a>
+               we seek to curate websites that minimize how many hands touch the income
+               of our clients while also offering affordable service.</p> <p class="transparent svelte-1hpexb2">filler</p> <p class="title svelte-1hpexb2" style="margin-left: -10%;">(Competition)</p> <p>Professional apperanceses are everything, from ensuring that missed
+            fees are payed to ensuring a smooth transaction of goods. Professional
+            apperances grant a buisness legitamacy, authority but most importantly
+            respect. A professional apperance adds weight to the pentalies placed
+            upon customers when fees are unmet thus ensuring businesses are made whole.</p></div></div> <img class="skull svelte-1hpexb2" src="${g}" alt="Img"/></div>`;return{c(){e=p("body"),e.innerHTML=a},l(t){e=c(t,"BODY",{"data-svelte-h":!0}),h(e)!=="svelte-bwgdrf"&&(e.innerHTML=a)},m(t,n){m(t,e,n)},p:s,i:s,o:s,d(t){t&&f(e)}}}class w extends l{constructor(e){super(),r(this,e,null,u,i,{})}}export{w as component};
