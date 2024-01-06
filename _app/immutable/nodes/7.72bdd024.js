d542a1369ea9f8ae60472b2ecd3a46f621b90ee9
ecruz22004 <ecruz22004@gmail.com>
2024-01-05 18:06:41 -0800

    Updates

---
+++ _app/immutable/nodes/7.72bdd024.js
@@ -0,0 +1,11 @@
+import{s as i,n as s}from"../chunks/scheduler.e108d1fd.js";import{S as l,i as r,g as p,h as c,x as m,a as f,f as h}from"../chunks/index.4d21fe7b.js";import{i as g}from"../chunks/logo5.3cfd03dd.js";import{b as u}from"../chunks/paths.f0568fe6.js";function d(n){let e,a=`<p class="top svelte-8n44m1">(Welcome)</p> <div class="containtop svelte-8n44m1"><div class="greycontainer svelte-8n44m1"><div class="boxtxt svelte-8n44m1"><p>Rotten Mangos is a Los Angeles marketing agency that seeks to promote 
+            and elevate local buisness by offering competitve yet affordable 
+            services that align with the interests of our clinetel.</p> <p class="transparent svelte-8n44m1">filler</p> <p class="title svelte-8n44m1">(Our philosophy)</p> <p>Local Businesses are now faced with insurmountable challenges when competing
+             against corporate America. By utilizing affordable and powerful
+             <a style="text-decoration:none; color:#DEC18F;" href="${u}/Archive">tools</a>
+               we seek to curate websites that minimize how many hands touch the income
+               of our clients while also offering affordable service.</p> <p class="transparent svelte-8n44m1">filler</p> <p class="title2 svelte-8n44m1">(Competition)</p> <p>Professional apperanceses are everything, from ensuring that missed
+            fees are payed to ensuring a smooth transaction of goods. Professional
+            apperances grant a buisness legitamacy, authority but most importantly
+            respect. A professional apperance adds weight to the pentalies placed
+            upon customers when fees are unmet thus ensuring businesses are made whole.</p></div></div> <img class="skull svelte-8n44m1" src="${g}" alt="Img"/></div>`;return{c(){e=p("body"),e.innerHTML=a},l(t){e=c(t,"BODY",{"data-svelte-h":!0}),m(e)!=="svelte-1qh6fgr"&&(e.innerHTML=a)},m(t,o){f(t,e,o)},p:s,i:s,o:s,d(t){t&&h(e)}}}class w extends l{constructor(e){super(),r(this,e,null,d,i,{})}}export{w as component};
