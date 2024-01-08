1b50327433cbbac4a5e485beffee49dc4eb81b25
ecruz22004 <ecruz22004@gmail.com>
2024-01-07 16:35:34 -0800

    Updates

---
+++ _app/immutable/chunks/paths.c41da80b.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1elo5sj)==null?void 0:s.base)??"/rottenmangos.xyz";var e;const a=((e=globalThis.__sveltekit_1elo5sj)==null?void 0:e.assets)??t;export{a,t as b};
