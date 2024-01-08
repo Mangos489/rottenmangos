4191ba562ff100a9e12d1104e5495f0e8ede10d9
ecruz22004 <ecruz22004@gmail.com>
2024-01-07 16:20:39 -0800

    Updates

---
+++ _app/immutable/nodes/12.8fdf0d3d.js
@@ -0,0 +1,12 @@
+import{s as o,n as a}from"../chunks/scheduler.e108d1fd.js";import{S as r,i as c,g as l,h as d,x as p,a as m,f as v}from"../chunks/index.4d21fe7b.js";const u=""+new URL("../assets/logo.5c5656c1.webp",import.meta.url).href;function f(i){let e,s=`<div class="containerorgin"><div class="containera"><div class="contentaa"><p class="title">(Website)</p> <p class="text">Websites are designed in a brief 15 min video call 
+                    to discuss goals and functionality. A quote is provided at the
+                     end for development and implementation.</p></div> <div class="contentab"><p class="title">(Database)</p> <p class="text">We design and build databases that empower your
+                     organization with efficient data management, secure data collection 
+                     and seamless integration with present applications.</p> <p class="filler">idk</p></div></div> <div class="containerb"><div class="contentba"><p class="title">(Maintenance)</p> <p class="text">If you require an extra set of hands to assist with 
+                    a project or application that fails to load assets, has slow render
+                    time, requires security checks or just assistance programming your
+                    project we have people ready to fulfil your needs at flat 
+                    hourly rates varying on your project&#39;s needs.</p></div> <div class="contentbb"><p class="title">(Integration)</p> <p class="text">We are able to integrate programs or services into exisiting websites or applications. This includes
+                    but is not limited to datatbases analytical tools and even artificial intelligence.
+                     Cost of service varies depending on the complexity and scale of integration.</p></div></div> <div class="containerthree"><p class="title">(Subscription)</p> <p class="text">A monthly subscription of $50 a month can be paid for priority service and a 50%
+                 discount on the hourly rate for Maintenance</p></div> <div class="containerimg"><img class="pic1" src="${u}" alt="Img"/></div></div>`;return{c(){e=l("body"),e.innerHTML=s},l(t){e=d(t,"BODY",{"data-svelte-h":!0}),p(e)!=="svelte-11fv6na"&&(e.innerHTML=s)},m(t,n){m(t,e,n)},p:a,i:a,o:a,d(t){t&&v(e)}}}class b extends r{constructor(e){super(),c(this,e,null,f,o,{})}}export{b as component};
