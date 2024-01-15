89e64e38278b6d09656ef79238893f7c3b2db527
ecruz22004 <ecruz22004@gmail.com>
2024-01-14 21:57:07 -0800

    Updates

---
+++ _app/immutable/chunks/paths.3f21ee12.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1veq53p)==null?void 0:s.base)??"/rottenmangos.xyz";var e;const a=((e=globalThis.__sveltekit_1veq53p)==null?void 0:e.assets)??t;export{a,t as b};
--- _app/immutable/nodes/10.acb88efe.js
+++
@@ -1,1 +0,0 @@
-import{s}from"../chunks/scheduler.e108d1fd.js";import{S as t,i as e}from"../chunks/index.a6586c94.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
