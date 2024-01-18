7deb49d7dfd350023fde1f9b6b15fff943e2b47a
ecruz22004 <ecruz22004@gmail.com>
2024-01-17 18:19:53 -0800

    Updates

---
+++ _app/immutable/nodes/11.f5894045.js
@@ -0,0 +1,12 @@
+import{s as o,f as c,g as r,u as l,i as d,z as a,d as p}from"../chunks/scheduler.c6dc58b5.js";import{S as v,i as m}from"../chunks/index.c4e83267.js";const u=""+new URL("../assets/logo.5c5656c1.webp",import.meta.url).href;function f(i){let e,s=`<div class="containerorgin"><div class="titlecontainer"><p class="titleone">(Products)</p></div> <div class="containera"><div class="contentaa"><p class="ytitle">(Website)</p> <p class="text">Websites are designed in a brief 15 min video call 
+                    to discuss goals and functionality. A quote is provided at the
+                     end for development and implementation.</p></div> <div class="contentab"><p class="ytitle">(Database)</p> <p class="text">We design and build databases that empower your
+                     organization with efficient data management, secure data collection 
+                     and seamless integration with present applications.</p></div></div> <div class="containerb"><div class="contentba"><p class="ytitle">(Maintenance)</p> <p class="text">If you require an extra set of hands to assist with 
+                    a project or application that fails to load assets, has slow render
+                    time, requires security checks or just assistance programming your
+                    project we have people ready to fulfil your needs at flat 
+                    hourly rates varying on your project&#39;s needs.</p></div> <div class="contentbb"><p class="ytitle">(Integration)</p> <p class="text">We are able to integrate programs or services into exisiting websites or applications. This includes
+                    but is not limited to datatbases analytical tools and even artificial intelligence.
+                     Cost of service varies depending on the complexity and scale of integration.</p></div></div> <div class="containerthree"><div class="contentc"><p class="ytitle">(Subscription)</p> <p class="text">A monthly subscription of $50 a month can be paid for priority service and a 50%
+                 discount on the hourly rate for Maintenance</p></div></div> <div class="containerimg"><img class="pic1" src="${u}" alt="Img"/></div></div>`;return{c(){e=c("body"),e.innerHTML=s},l(t){e=r(t,"BODY",{"data-svelte-h":!0}),l(e)!=="svelte-ja6jsp"&&(e.innerHTML=s)},m(t,n){d(t,e,n)},p:a,i:a,o:a,d(t){t&&p(e)}}}class y extends v{constructor(e){super(),m(this,e,null,f,o,{})}}export{y as component};
