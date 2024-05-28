abd3ad8a1fed2c855b1412cdf1b45190c6f0f10e
ecruz22004 <ecruz22004@gmail.com>
2024-05-27 19:26:15 -0700

    Updates

---
+++ _app/immutable/nodes/7.644dd3d4.js
@@ -0,0 +1,10 @@
+import{s as i,f as l,g as r,u as p,i as c,z as s,d as m}from"../chunks/scheduler.c6dc58b5.js";import{S as f,i as d}from"../chunks/index.c4e83267.js";import{i as u}from"../chunks/logo5.3cfd03dd.js";import{b as g}from"../chunks/paths.50616e74.js";function h(n){let e,a=`<p class="top svelte-8n44m1">(Welcome)</p> <div class="containtop svelte-8n44m1"><div class="greycontainer svelte-8n44m1"><div class="boxtxt svelte-8n44m1"><p>Rotten Mangos is a Los Angeles marketing agency that seeks to promote 
+            and elevate local business by offering competitive yet affordable services that align with the interests of our clientele.</p> <p class="transparent svelte-8n44m1">filler</p> <p class="title svelte-8n44m1">(Our philosophy)</p> <p>Local Businesses are now faced with insurmountable challenges when competing
+             against corporate America. By utilizing affordable and powerful
+             <a style="text-decoration:none; color:#DEC18F;" href="${g}/products">tools</a>
+               we seek to curate websites that minimize how many hands touch the income
+               of our clients while also offering affordable service.</p> <p class="transparent svelte-8n44m1">filler</p> <p class="title2 svelte-8n44m1">(Competition)</p> <p>Professional appearances are everything, from ensuring that missed fees
+            are paid to ensuring a smooth transaction of goods. Professional
+            appearances grant a business legitimacy, authority but most importantly
+            respect. A professional appearance adds weight to the penalties placed 
+            upon customers when fees are unmet thus ensuring businesses are made whole.</p></div></div> <img class="skull svelte-8n44m1" src="${u}" alt="Img"/></div>`;return{c(){e=l("body"),e.innerHTML=a},l(t){e=r(t,"BODY",{"data-svelte-h":!0}),p(e)!=="svelte-x3vp9d"&&(e.innerHTML=a)},m(t,o){c(t,e,o)},p:s,i:s,o:s,d(t){t&&m(e)}}}class w extends f{constructor(e){super(),d(this,e,null,h,i,{})}}export{w as component};
